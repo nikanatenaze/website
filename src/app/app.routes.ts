31abc995c8a15279a11c1db43dca703e73141bb8
Nika Natenadze <nikanatenaze@gmail.com>
2026-06-14 16:58:05 +0400

    upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home')
+        .then(x => x.Home)
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home')
+        .then(x => x.Home)
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/error/error')
+        .then(x => x.Error)
+  }
+];
